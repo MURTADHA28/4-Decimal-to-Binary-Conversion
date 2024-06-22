8b0cfa47fcb39d0c6c5c0044962693f530cb4f48
MURTADHA ALBAHRANI <160097883+MURTADHA28@users.noreply.github.com>
2024-06-22 09:53:40 +0300

    Update . DecimaltoBinary.php

--- . DecimaltoBinary.php
+++ . DecimaltoBinary.php
@@ -1,1 +1,20 @@
 
+function decimalToBinary($number) {
+    if ($number == 0) {
+        return "0";
+    }
+
+    $binaryString = '';
+    while ($number > 0) {
+        $remainder = $number % 2;
+        $binaryString = $remainder . $binaryString;
+        $number = (int)($number / 2);
+    }
+
+    return $binaryString;
+}
+
+
+$decimalNumber = 30;
+$binaryResult = decimalToBinary($decimalNumber);
+echo "Binary of $decimalNumber is $binaryResult";
